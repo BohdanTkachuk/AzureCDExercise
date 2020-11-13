e76aa2cdf01e532fd3051e715a8c06d27fa051c8
BohdanTkachuk <49440230+BohdanTkachuk@users.noreply.github.com>
2020-11-13 14:49:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home, new commit (CI)</h3>"
+    html = "<h3>Sklearn Prediction Home, new commit (CI) new try</h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
